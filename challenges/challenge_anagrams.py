999abb61816f7ddfefdea8f8811b0c7bae677fb4
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2022-07-10 11:24:11 -0300

    anagrams

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,11 @@
 def is_anagram(first_string, second_string):
     """ Faça o código aqui. """
+    first = list(first_string.lower())
+    second = list(second_string.lower())
+
+    for elem in first:
+        if elem in second:
+            return second.remove(elem)
+        return False
+
+    return len(second) == 0

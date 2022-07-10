8fb17b8d10f887560e22533f215dd971e196a689
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2022-07-10 10:49:28 -0300

    study schedule,

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,15 @@
 # starting project
 def study_schedule(permanence_period, target_time):
     """ Faça o código aqui. """
+    if target_time is None:
+        return None
+
+    students_number = 0
+
+    for entry, out in permanence_period:
+        if type(entry) != int or type(out) != int:
+            return None
+        if entry <= target_time <= out:
+            students_number +=1
+    
+    return students_number
